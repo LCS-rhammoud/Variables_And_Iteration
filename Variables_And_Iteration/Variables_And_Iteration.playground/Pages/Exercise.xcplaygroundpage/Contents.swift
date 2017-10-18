b5811279218e52ec36cd6270bc6804407cf7552e
Hammoud Razan <rhammoud@lcs-mba3113.lcs.on.ca>
2017-10-18 11:42:35 -0400

    done

--- Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ Variables_And_Iteration/Variables_And_Iteration.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -11,24 +11,73 @@
 //: ## Required code
 //: The following two statements are required to make the playground run. Please do not remove.
 import Cocoa
+
 import PlaygroundSupport
+
+
+
 // Create canvas
+
 let canvas = Canvas(width: 300, height: 300)
 
 
-//: ## Add your code below
-//draw Circles
-for x in stride(from: 0, through: 300, by: 30)  {
-    for y in stride(from: 0, to: 300, by: 30)  {
+
+for x in stride(from: 0, through: 300, by: 25)  {
+    
+    for y in stride(from: 0, through: 300, by: 25) {
+        
         canvas.drawEllipse(centreX: x, centreY: y, width: 2, height: 2)
         
     }
+    
 }
 
+canvas.fillColor = Color.init(hue: 30, saturation: 0, brightness: 25, alpha: 90)
+
+canvas.drawRectangle(centreX: 0, centreY: 0, width: 1000, height: 250)
 
 
-/*:
- ## Template code
- The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
- */
-PlaygroundPage.current.liveView = canvas.imageView
+
+canvas.fillColor = Color.init(hue: 30, saturation: 0, brightness: 30, alpha: 100)
+
+canvas.drawRectangle(centreX: 0, centreY: 0, width: 1000, height: 100)
+
+
+
+//draw mountain peek
+
+for i in 0...50 {
+    
+    canvas.drawLine(fromX: i, fromY: 125, toX: 50+i, toY: 175-i)
+    
+    canvas.drawLine(fromX: 100+i, fromY: 125, toX: 150+i, toY: 175-i)
+    
+    canvas.drawLine(fromX: 200+i, fromY: 125, toX: 250+i, toY: 175-i)
+    
+    
+    
+}
+
+// draw lower mountains
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+PlaygroundPage.current.liveView = canvas.imageView   
